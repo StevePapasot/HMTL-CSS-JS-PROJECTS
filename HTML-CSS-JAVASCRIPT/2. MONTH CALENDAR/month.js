8729a1ea69cd96389b0534138d44a4cfc2554059
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2023-09-28 16:37:06 +0300

    Add files via upload

--- HTML-CSS-JAVASCRIPT/2. MONTH CALENDAR/month.js
+++ HTML-CSS-JAVASCRIPT/2. MONTH CALENDAR/month.js
@@ -1,5 +1,11 @@
-const monthInx = new Date().getMonth;
+const monthEl = document.querySelector(".date h1");
+const dayEl = document.querySelector(".date p");
+const daysEl = document.querySelector(".days");
 
+
+const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(),monthInx + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(),monthInx, 1).getDay() - 1;
 
 const months = [
     "January", 
@@ -16,3 +22,20 @@ const months = [
     "December", 
 ]
 
+monthEl.innerText = months[monthInx];
+dayEl.innerText = new Date().toDateString();
+
+let days = "";
+
+for(let i = firstDay; i > 0; i--){
+    days += `<div class="empty"></div>`;
+}
+
+for (let i=1; i<=lastDay; i++){
+    if( i === new Date().getDate()){
+        days+=`<div class="today">${i}</div>`;
+    }
+    days += `<div>${i}</div>`;
+}
+
+daysEl.innerHTML = days;
